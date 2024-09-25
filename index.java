02ea1f84fd8502c5fff0dff564cb836150a20b11
gandy <162331163+GandyZinanyuca@users.noreply.github.com>
2024-09-25 11:47:57 -0500

    titulo mejorado

--- index.java
+++ index.java
@@ -2,7 +2,7 @@ package edu.pucp.recursos.main.model;
 import edu.pucp.personal.model.persona;
 import edu.pucp.personal.model.empleado;
 public static void main(String[] args) {
-    System.out.println("nuevo archivo");
+    System.out.println("nuevas actualizaciones");
     empleado personal1 = new empleado("elias", 34, 78.5,1);
     personal1.muestraDatos();
 }
