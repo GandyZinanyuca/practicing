7d85c2076067ef700ea4b6eeb22081f7952d08a3
gandy <162331163+GandyZinanyuca@users.noreply.github.com>
2024-09-25 11:51:53 -0500

    re insersión

--- index.java
+++ index.java
@@ -1,6 +1,7 @@
 package edu.pucp.recursos.main.model;
 import edu.pucp.personal.model.persona;
 import edu.pucp.personal.model.empleado;
+
 public static void main(String[] args) {
     System.out.println("nuevas actualizaciones");
     empleado personal1 = new empleado("elias", 34, 78.5,1);
