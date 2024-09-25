a96ac6048d19d0b82895b7bd3664587d794966e7
gandy <162331163+GandyZinanyuca@users.noreply.github.com>
2024-09-25 11:53:08 -0500

    espacios

--- index.java
+++ index.java
@@ -5,5 +5,9 @@ import edu.pucp.personal.model.empleado;
 public static void main(String[] args) {
     System.out.println("nuevas actualizaciones");
     empleado personal1 = new empleado("elias", 34, 78.5,1);
+
+
+
+    
     personal1.muestraDatos();
 }
